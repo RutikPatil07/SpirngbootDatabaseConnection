237756971143962e84b04235bed0211833b42c20
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-13 12:25:11 +0530

    Create punedb1application.java

---
+++ punedb1application.java
@@ -0,0 +1,14 @@
+package com.cs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Punedbcon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Punedbcon1Application.class, args);
+		System.out.println("Success.....!!!!");
+	}
+
+}
