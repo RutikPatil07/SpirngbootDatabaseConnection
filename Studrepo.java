9f17fc18a64a8dbef3bd63ed30d96bfa68ca093e
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-16 23:21:23 +0530

    Add methods to count total and max students

--- Studrepo.java
+++ Studrepo.java
@@ -1,9 +1,16 @@
 package com.cs.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cs.demo.model.Stud;
 
 public interface Studrepo extends JpaRepository<Stud, Integer>{
 
+	 @Query(value = "SELECT COUNT(*) FROM stud", nativeQuery = true)
+	 Long getTotalStud();
+	 
+	 @Query(value = "SELECT COUNT(*) FROM stud", nativeQuery = true)
+	 Long getaxStud();
 }
+    
