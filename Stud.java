389d2e1b4c8d2a1015f0e0f5457817a211af9e9a
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-16 23:22:28 +0530

    Update Stud.java

--- Stud.java
+++ Stud.java
@@ -3,14 +3,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Stud {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 private int id;
+@NotBlank(message = "Name is required")	 
 private String name;
+@NotBlank(message = "City is required")	
 private String city;
+@NotBlank(message = "Mobile is required")	
 private String mob;
 public int getId() {
 	return id;
@@ -29,6 +33,12 @@ public String getCity() {
 }
 public void setCity(String city) {
 	this.city = city;
+}
+public String getMob() {
+	return mob;
+}
+public void setMob(String mob) {
+	this.mob = mob;
 }
 
 
